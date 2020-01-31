8aff98edac22873b55747bb1f8183217a2e847b5
Potzy12323123125y7 <alexander.naslund2@elev.ga.ntig.se>
2020-01-31 11:16:15 +0100

    2020-01-31

--- src/Komplexatal.java
+++ src/Komplexatal.java
@@ -1,4 +1,19 @@
+// Alexander Jonsson Näslund
+
 public class Komplexatal {
     private double a;
     private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public Komplexatal(double aa, double bb) {
+        this.a = aa;
+        this.b = bb;
+    }
 }
